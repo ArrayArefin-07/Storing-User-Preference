7c52788d881d5d87f3cc8284af3f4f3624eae31c
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-10-05 19:45:11 +0600

    added event listener for font size
    & added function for event listener

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ const selectFontSize = document.getElementById("selectFontSize");
 const selectBgColor = document.getElementById("selectBgColor");
 const resetButton = document.getElementById("resetButton");
 const mainElement = document.querySelector("main");
+
+
+// change font size function
+const changeFontSize = (event) => {
+  console.log(event.target.value);
+}
+
+
+// add event listeners 
+selectFontSize.addEventListener("change", changeFontSize );
