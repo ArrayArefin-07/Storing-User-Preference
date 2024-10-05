d460e3f8435f8bb00de69e805af1a7ed2b48485b
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-10-05 16:21:33 +0600

    find element in js file

---
+++ index.js
@@ -0,0 +1,6 @@
+// find element
+const selectFontSize = document.getElementById("selectFontSize");
+
+const selectBgColor = document.getElementById("selectBgColor");
+const resetButton = document.getElementById("resetButton");
+const mainElement = document.querySelector("main");
